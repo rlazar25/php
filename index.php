0642dba5091f1018f009de98b80336dfb2c4d258
rlazar25 <119755392+rlazar25@users.noreply.github.com>
2024-05-13 19:09:27 +0200

    Dodat pets.php i tabela u index.php

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
+<?php require('pets.php'); ?>
 <?php require('partials/top.php'); ?>
     <h1>Welcome To PetShop</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-6 offset-3">
+                <table class="table table-dark">
+                    <thead>
+                        <tr>
+                            <th>Pet</th>
+                            <th>Type</th>
+                            <th>Owner</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach($pets as $pet): ?>
+                            <tr>
+                                <td>
+                                    <?php echo $pet["name"] ?>
+                                </td>
+                                <td>
+                                    <?php echo $pet["type"] ?>
+                                </td>
+                                <td>
+                                    <?php echo $pet["owner"] ?>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 <?php require('partials/bottom.php'); ?>
---
+++ pets.php
@@ -0,0 +1,21 @@
+<?php 
+    $pets = [
+         [
+            'name' => "Fifi",
+            "type" => "dog",
+            "owner" => "Danilo",
+            "info" => "Lorem Fifi, ipsum dolor sit amet consectetur adipisicing elit. Magni maiores sunt id fugit totam officia aspernatur nobis dolore assumenda quas?"
+         ],
+         [
+            'name' => "Max",
+            "type" => "dog",
+            "owner" => "Ivan",
+            "info" => "Lorem Max, ipsum dolor sit amet consectetur adipisicing elit. Magni maiores sunt id fugit totam officia aspernatur nobis dolore assumenda quas?"
+         ],[
+            'name' => "Nemo",
+            "type" => "Fish",
+            "owner" => "Slavica",
+            "info" => "Lorem Nemo, ipsum dolor sit amet consectetur adipisicing elit. Magni maiores sunt id fugit totam officia aspernatur nobis dolore assumenda quas?"
+         ]
+    ]
+?>
